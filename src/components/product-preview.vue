9989cce1653c55205fec8c1280dd5c2de637b70e
Idan Maimon <idanmaimon381891@gmail.com>
2022-12-08 21:44:22 +0200

    product-preview ui & demo data

--- src/components/product-preview.vue
+++ src/components/product-preview.vue
@@ -2,6 +2,7 @@
 <template>
     <section class="product-preview-layout" @click.prevent="showDetails">
         <img :src="getImgUrl(currProduct.imgs[0])">
+        <div class="product-date">{{ getEditedAtDate }}</div>
         <div class="product-min-info">
             <div class="min-info-title">{{ currProduct.title }}</div>
             <div class="min-info-price">{{ currProduct.price }}</div>
@@ -19,7 +20,17 @@ export default {
         return {
         }
     },
-    computed: {},
+    computed: {
+        getEditedAtDate() {
+            const date = new Date(this.currProduct.editedAt)
+            const yy = (date.getFullYear() + '').slice(-2)
+            let mm = date.getMonth() + 1
+            let dd = date.getDate()
+            if (dd < 10) dd = '0' + dd
+            if (mm < 10) mm = '0' + mm
+            return dd + '/' + mm + '/' + yy
+        }
+    },
     methods: {
         showDetails() {
             this.$router.push({ path: `/product/${this.currProduct._id}` })
