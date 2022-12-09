cf333cf9a6c4f7d476afced1fddcbe70930267e4
Idan Maimon <idanmaimon381891@gmail.com>
2022-12-10 00:38:20 +0200

    added demo data

--- src/store/modules/product-module.js
+++ src/store/modules/product-module.js
@@ -14,7 +14,7 @@ export default {
       description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere et rerum, nesciunt aliquid quos laudantium possimus? Voluptate, exercitationem, facilis saepe rem error modi fugit optio necessitatibus maxime dolorem consequuntur tenetur", seller: { _id: "demoSeller", name: "demoseller", phone: "0541111111", }
     }, {
       _id: "demoProd3", condition: "as new", title: "stewie griffin necklace", price: "140$",
-      editedAt: 1670540830062, imgs: ["1100.jpg", "1101.jpg", "1102.jpg", "1103.jpg"], tags: ["ring", "jewelry", "necklace"],
+      editedAt: 1670540830062, imgs: ["1100.jpg", "1101.jpg", "1102.jpg", "1103.jpg"], tags: ["stainless", "jewelry", "necklace"],
       category: "jewelry", location: { country: "israel", city: "tel aviv", },
       description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere et rerum, nesciunt aliquid quos laudantium possimus? Voluptate, exercitationem, facilis saepe rem error modi fugit optio necessitatibus maxime dolorem consequuntur tenetur", seller: { _id: "demoSeller", name: "demoseller", phone: "0541111111", }
     }, {
@@ -33,16 +33,16 @@ export default {
       category: "jewelry", location: { country: "israel", city: "ramat gan", },
       description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere et rerum, nesciunt aliquid quos laudantium possimus? Voluptate, exercitationem, facilis saepe rem error modi fugit optio necessitatibus maxime dolorem consequuntur tenetur", seller: { _id: "demoSeller", name: "demoseller", phone: "0541111111", }
     }, {
-      _id: "demoProd7", condition: "as new", title: "dali's clock", price: "22$",
-      editedAt: 1670421619145, imgs: ["1096.jpg", "1097.jpg", "1098.jpg", "1099.jpg"], tags: ["art", "watch", "clock"],
-      category: "art & decoration", location: { country: "israel", city: "tel aviv", },
-      description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere et rerum, nesciunt aliquid quos laudantium possimus? Voluptate, exercitationem, facilis saepe rem error modi fugit optio necessitatibus maxime dolorem consequuntur tenetur", seller: { _id: "demoSeller", name: "demoseller", phone: "0541111111", }
-    }, {
-      _id: "demoProd8", condition: "new", title: "Vertical mouse", price: "26$",
+      _id: "demoProd7", condition: "new", title: "Vertical mouse", price: "26$",
       editedAt: 1670421619145, imgs: ["vertical-mouse.jpg", "vertical-mouse2.jpg", "vertical-mouse3.jpg", "vertical-mouse4.jpg"], tags: ["mouse", "tech", "bluetooth"],
       category: "mouses & keyboards", location: { country: "israel", city: "haifa", },
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo sed animi molestias quia ad ex necessitatibus expedita consequatur recusandae deserunt, at error porro architecto aspernatur provident neque eum impedit dolore.",
       seller: { _id: "demoSeller", name: "demoseller", phone: "0541111111", }
+    }, {
+      _id: "demoProd8", condition: "as new", title: "dali's clock", price: "22$",
+      editedAt: 1670421619145, imgs: ["1096.jpg", "1097.jpg", "1098.jpg", "1099.jpg"], tags: ["art", "watch", "clock"],
+      category: "art & decoration", location: { country: "israel", city: "tel aviv", },
+      description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere et rerum, nesciunt aliquid quos laudantium possimus? Voluptate, exercitationem, facilis saepe rem error modi fugit optio necessitatibus maxime dolorem consequuntur tenetur", seller: { _id: "demoSeller", name: "demoseller", phone: "0541111111", }
     }, {
       _id: "demoProd9", condition: "new", title: "god of war pop figure", price: "29$",
       editedAt: 1670321619145, imgs: ["1104.jpg", "1105.jpg", "1106.jpg", "1107.jpg"], tags: ["pop", "figure", "hobbies"],
@@ -55,8 +55,13 @@ export default {
       description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Assumenda, commodi recusandae officia suscipit voluptatibus aut soluta! Repudiandae eaque quam facere esse assumenda facilis suscipit.Alias vero error magni ab quam.",
       seller: { _id: "demoSeller", name: "demoseller", phone: "0541111111", }
     }, {
-      _id: "demoProd11", condition: "as new", title: "metal-detector", price: "90$",
-      editedAt: 1670321619145, imgs: ["metal-detector.jpg", "metal-detector2.jpg", "metal-detector3.jpg", "metal-detector4.jpg"], tags: ["demotag", "demotag2", "demotag3"],
+      _id: "demoProd11", condition: "as new", title: "chained shark necklace", price: "30$",
+      editedAt: 1670321619145, imgs: ["1112.jpg", "1113.jpg", "1114.jpg", "1115.jpg"], tags: ["stainless", "jewelry", "necklace"],
+      category: "jewelry", location: { country: "israel", city: "tel aviv", },
+      description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere et rerum, nesciunt aliquid quos laudantium possimus? Voluptate, exercitationem, facilis saepe rem error modi fugit optio necessitatibus maxime dolorem consequuntur tenetur", seller: { _id: "demoSeller", name: "demoseller", phone: "0541111111", }
+    }, {
+      _id: "demoProd12", condition: "as new", title: "metal-detector", price: "90$",
+      editedAt: 1670211619145, imgs: ["metal-detector.jpg", "metal-detector2.jpg", "metal-detector3.jpg", "metal-detector4.jpg"], tags: ["demotag", "demotag2", "demotag3"],
       category: "hobbies", location: { country: "israel", city: "ashkelon", },
       description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere et rerum, nesciunt aliquid quos laudantium possimus? Voluptate, exercitationem, facilis saepe rem error modi fugit optio necessitatibus maxime dolorem consequuntur tenetur", seller: { _id: "demoSeller", name: "demoseller", phone: "0541111111", }
     }]
