1bf489eead8002ed96d984700375b614e7604cc3
Idan Maimon <idanmaimon381891@gmail.com>
2022-12-16 11:44:00 +0200

    filter

--- src/components/side-filters.vue
+++ src/components/side-filters.vue
@@ -2,9 +2,30 @@
 <template>
     <section class="side-filters-layout">
         <section class="side-filters-cont">
-            categories
-            <div v-for="(category, idx) in this.categories"
-                @click="setFilter({ category: category, price: { min: 0, max: 0 } })" :key="category">{{ category }}</div>
+            <section class="side-filters-price">
+                <h2>Price Range</h2>
+                <div class="min-price-inputs">
+                    <input type="number" id="min-price" min="0" max="150" v-model="filter.price.min" @change="setFilter()">
+                    <input type="range" id="min-price-range" class="slider" min="0" max="150"
+                        v-model="filter.price.min" @change="setFilter()">
+                </div>
+                <div class="max-price-inputs">
+                    <input type="number" id="min-price" min="1" max="150" v-model="filter.price.max" @change="setFilter()">
+                    <input type="range" id="max-price-range" class="slider" min="1" max="150"
+                        v-model="filter.price.max" @change="setFilter()">
+                </div>
+            </section>
+            <section class="side-filters-categories">
+                <div @click="filter.category = ''; setFilter();">categories</div>
+                <div v-for="(category, idx) in this.categories" @click="filter.category = category; setFilter();"
+                    :key="idx + 7">
+                    {{ category }}
+                </div>
+            </section>
+        </section>
+    </section>
+    <section class="side-filters-layout-mobile" v-if="isBigCmp">
+        <section class="side-filters-cont-mobile">
         </section>
     </section>
 </template>
@@ -14,16 +35,19 @@ export default {
     data() {
         return {
             categories: ["art & decoration", "gadgets", "hobbies", "jewelry", "watches"],
+            isBigCmp: false,
+            filter: { category: '', price: { min: 0, max: 150, } },
         }
     },
     computed: {
     },
     methods: {
-        setFilter(filter) {
+        setFilter() {
+            console.log('this.filter: ',this.filter)
             var filterBy = Object.assign({}, this.$store.getters.filterBy)
-            filterBy.category = filter?.category || ''
-            filterBy.minPrice = filter?.price.min || 0
-            filterBy.maxPrice = filter?.price.max || 1000
+            filterBy.category = this.filter?.category || ''
+            filterBy.minPrice = this.filter?.price?.min || 0
+            filterBy.maxPrice = this.filter?.price?.max || 150
             this.$store.dispatch({ type: "setFilter", filterBy })
             // this.setQuery(filterBy)
         },
